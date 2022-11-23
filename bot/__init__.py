2226ad19571c76b6412f147cb8dc6754952aed59
Juned KH <kjuned007@gmail.com>
2022-11-23 20:25:51 +0530

    Major Change Fix:User string issue with leech Add:Send message when upload finished

--- bot/__init__.py
+++ bot/__init__.py
@@ -156,6 +156,7 @@ if len(EXTENSION_FILTER) > 0:
         GLOBAL_EXTENSION_FILTER.append(x.strip().lower())
 
 IS_PREMIUM_USER = False
+IS_USER_SESSION = False
 USER_SESSION_STRING = environ.get('USER_SESSION_STRING', '')
 if len(USER_SESSION_STRING) == 0:
     log_info("Creating client from BOT_TOKEN")
@@ -167,6 +168,7 @@ else:
                  session_string=USER_SESSION_STRING, parse_mode=enums.ParseMode.HTML, no_updates=True)
     with app:
         IS_PREMIUM_USER = app.me.is_premium
+    IS_USER_SESSION = True
 
 RSS_USER_SESSION_STRING = environ.get('RSS_USER_SESSION_STRING', '')
 if len(RSS_USER_SESSION_STRING) == 0:
--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait, RPCError
 from PIL import Image
 from threading import RLock
 
-from bot import config_dict, user_data, app, GLOBAL_EXTENSION_FILTER
+from bot import config_dict, user_data, app, GLOBAL_EXTENSION_FILTER, IS_USER_SESSION
 from bot.helper.ext_utils.fs_utils import take_ss, get_media_info, get_media_streams, clean_unwanted
 from bot.helper.ext_utils.bot_utils import get_readable_file_size
 
@@ -152,6 +152,17 @@ class TgUploader:
                                                                  disable_notification=True,
                                                                  reply_markup=self.__button,
                                                                  progress=self.__upload_progress)
+            if self.__listener.dmMessage:
+                if IS_USER_SESSION:
+                    self.__sent_DMmsg = self.__listener.bot.copy_message(
+                    chat_id=self.__listener.message.from_user.id,
+                    from_chat_id=self.__sent_msg.chat.id,
+                    message_id=self.__sent_msg.id,
+                    reply_to_message_id=self.__sent_DMmsg['message_id'])
+                else:
+                    self.__sent_DMmsg = self.__sent_msg.copy(
+                        chat_id=self.__sent_DMmsg.chat.id,
+                        reply_to_message_id=self.__sent_DMmsg.id)
         except FloodWait as f:
             LOGGER.warning(str(f))
             sleep(f.value)
@@ -192,14 +203,14 @@ class TgUploader:
             self.__thumb = None
 
     def __msg_to_reply(self):
-        if self.__listener.dmMessage:
-            self.__sent_msg = app.get_messages(self.__listener.message.from_user.id, self.__listener.dmMessage.message_id)
-        elif DUMP_CHAT:= config_dict['DUMP_CHAT']:
-            if self.__listener.isPrivate:
-                msg = self.__listener.message.text
-            else:
-                msg = self.__listener.message.link
+        if DUMP_CHAT:= config_dict['DUMP_CHAT']:
+            msg = self.__listener.message.text if self.__listener.isPrivate else self.__listener.message.link
             self.__sent_msg = app.send_message(DUMP_CHAT, msg, disable_web_page_preview=True)
+            if self.__listener.dmMessage:
+                if IS_USER_SESSION:
+                    self.__sent_DMmsg = {'message_id' : self.__listener.dmMessage.message_id}
+                else:
+                    self.__sent_DMmsg = app.get_messages(self.__listener.message.from_user.id, self.__listener.dmMessage.message_id)
         else:
             self.__sent_msg = app.get_messages(self.__listener.message.chat.id, self.__listener.uid)
         if self.__listener.message.chat.type != 'private' and not self.__listener.dmMessage:
